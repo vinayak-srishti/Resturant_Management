94110051d63578f91f463b8a156e787c0ab062ec
Ammu-raj <ammuamz2001@gmail.com>
2024-01-18 21:03:27 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import UserRegistration from './Components/User/UserRegistration'
+// import UserRegistration from './Components/User/UserRegistration'
+import Footer from './Components/User/Footer';
 
 function App() {
   return (
     <div>
-      <UserRegistration/>
+      {/* <UserRegistration/> */}
+      <Footer/>
     </div>
   )
 }
--- src/Components/User/UserRegistration.js
+++ src/Components/User/UserRegistration.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../User/UserRegistration.css'
+import validator from 'validator'
 
 function UserRegistration() {
+  const[form,setform]=useState({
+    firstname:"",
+    lastname:"",
+    contactno:"",
+    email:"",
+    newpassword:"",
+    confirmpassword:"",
+    address:"",
+    district:"",
+    state:"",
+    pincode:""
+  })
+
+  const[errormsg,seterrormsg]=useState('')
+  const validate=(value)=>{
+    if(validator.isStrongPassword(value,{
+     minLength:8,minLowercase:1,minUppercase:1,minNumbers:1,minSymbols:1
+    })){
+      seterrormsg('strong password');
+    }else{
+      seterrormsg('not strong password')
+    }
+  }
+  const handleclickfunction=(e)=>{
+    setform({...form,[e.target.name]:[e.target.value]})
+  }
+  const onSubmitdata=(e)=>{
+    e.preventDefault()
+    console.log(form)
+    console.log("submitted")
+  }
   return (
 <div className='userregcustomer-form'>
         <center>
-        <form>
+        <form onSubmit={onSubmitdata}>
           
           <h1>REGISTRATION</h1>
 
@@ -13,39 +45,41 @@ function UserRegistration() {
         <div className='userregcustomer-name'>
         <div className='row gt-2'>
         <div className='col-auto'>
-        <input type='text' placeholder='First Name' className='form-control'  />
+        <input type='text' placeholder='First Name' name='firstname' onChange={handleclickfunction} className='form-control' required />
         </div>
         <div className='col-auto'>
-        <input type='text' placeholder='Last Name' className='form-control'/>
+        <input type='text' placeholder='Last Name' name='lastname' onChange={handleclickfunction} className='form-control' required/>
         </div>
         </div>
         </div>
         
-        <div className='userregdob'>
-        <input type='date' placeholder='date of birth' className='form-control'/>
-        </div>
 
-
-      <div className='userregno-email'>
-      <input type='text' placeholder='Contact No' className='form-control'/><br/>
-      <input type='text' placeholder='Email' className='form-control' /><br/>
-      <input type='text' placeholder=' Address' className='form-control'/><br/>
-      </div>
+      <div className='userregno-email-pass'>
+      <input type='text' placeholder='Contact No' name='contactno' onChange={handleclickfunction} className='form-control' required/><br/>
+      <input type='text' placeholder='Email' name='email' onChange={handleclickfunction} className='form-control' required/><br/>
+      <input type='password' placeholder=' Password' onChange={(e) => validate(e.target.value)} name='newpassword' className='form-control' required/>
+      {errormsg === '' ? null :<span style={{  fontWeight: 'bold',color: 'red',}}>{errormsg}</span>}
+     </div>
     
-     <div className='userregdistrict-state'>
+     <div className='userreg-address'>
+     <input type='text' placeholder=' Address' name='address' onChange={handleclickfunction} className='form-control' required/><br/> 
+     </div>  
+
+      <div className='userreg-district-state'>  
       <div className='row gt-2'>
       <div className='col-auto'>
-      <input type='text' placeholder='City/District' className='form-control'/>
+      <input type='text' placeholder='City/District' onChange={handleclickfunction} name='district' className='form-control' required/>
       </div>
       <div className='col-auto'>
-     <input type='text' placeholder='State' className='form-control' /><br/>
+     <input type='text' placeholder='State' name='state' onChange={handleclickfunction} className='form-control' required /><br/>
      </div>
      </div>
      </div>
+     
 
 
      <div className='userregpincode'>
-      <input type='text' placeholder='Pincode' className='form-control'/>
+      <input type='text' placeholder='Pincode' name='pincode' onChange={handleclickfunction} className='form-control' required/>
      </div>
      <button type='button' className='btn btn-danger'>Register</button><br/>
      </div>
