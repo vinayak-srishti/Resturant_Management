dd77f83cd413f49bc577909c4eb05dab95c0795e
Ammu-raj <ammuamz2001@gmail.com>
2024-01-18 21:11:54 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-// import UserRegistration from './Components/User/UserRegistration'
-import Footer from './Components/User/Footer';
+import UserRegistration from './Components/User/UserRegistration'
+// import Footer from './Components/User/Footer';
 
 function App() {
   return (
     <div>
-      {/* <UserRegistration/> */}
-      <Footer/>
+      <UserRegistration/>
+      {/* <Footer/> */}
     </div>
   )
 }
