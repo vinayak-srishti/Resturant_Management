91232be42b9a388e4012d00825593588dc95ed49
jazi300 <laljazim20@gmail.com>
2024-03-22 16:03:20 +0530

    commit1

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
   return (
     <BrowserRouter>
     <div>
-      {/* <Adminlogin/> */}
+      <Adminlogin/>
       <Navbar/>
        <Routes> 
       {/*******************************User************************************/}
